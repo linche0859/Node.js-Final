c0034955472697939f43f8b0736e0de0339334fd
che.lin <aa10327tw@gmail.com>
2022-04-09 15:59:27 +0800

    feat: 登入和註冊頁

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,23 @@
 module.exports = {
   content: ['./src/**/*.{html,js}'],
   theme: {
-    extend: {},
+    fontFamily: {
+      paytone: ['"Paytone One"', 'sans-serif'],
+      helvetica: ['"Helvetica Neue"', 'sans-serif'],
+      azeret: ['"Azeret Mono"', 'sans-serif'],
+    },
+    extend: {
+      colors: {
+        primary: '#03438D',
+        black: {
+          100: '#000400',
+        },
+        gray: {
+          100: '#808080',
+          200: '#A8B0B9',
+        },
+      },
+    },
   },
   plugins: [],
 };
